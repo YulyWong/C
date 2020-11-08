05d0ef841b4c5daf15906aed8eeca27e42e2e56f
YulyWong <1219579537@qq.com>
2020-11-09 00:17:05 +0800

    2020_11_7

---
+++ 2020_11_7/2020_11_7/test.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+//¼ÆËãnµÄ½×³Ë
+int main()
+{
+int i=0;
+int n = 0;
+int sum = 1;
+scanf("%d", &n);
+for (i = 1; i <= n; i++)
+{
+	sum *= i;
+}
+printf("%d", sum);
+
+
+return 0;
+}
